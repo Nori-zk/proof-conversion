8321eb1c2ab67c0012d06edfd72d92d535f0ec3c
0x471 <0x471@protonmail.com>
2025-07-26 16:11:12 +0100

    feat(groth16): make zkp14/zkp15 circuits support dynamic inputs

--- src/groth/recursion/zkp14.ts
+++ src/groth/recursion/zkp14.ts
@@ -5,6 +5,7 @@ import { ArrayListHasher } from '../../array_list_hasher.js';
 import { VK } from '../vk_from_env.js';
 import { G1Affine } from '../../ec/index.js';
 import { bn254 } from '../../ec/g1.js';
+import { CONFIG } from '../config.js';
 
 const zkp14 = ZkProgram({
   name: 'zkp14',
@@ -12,20 +13,53 @@ const zkp14 = ZkProgram({
   publicOutput: Field,
   methods: {
     compute: {
-      privateInputs: [Provable.Array(FrC.provable, 5)],
+      privateInputs: [Provable.Array(FrC.provable, CONFIG.publicInputCount)],
       async method(input: Field, pis: Array<FrC>) {
         const pis_hash = Poseidon.hashPacked(
-          Provable.Array(FrC.provable, 5),
+          Provable.Array(FrC.provable, CONFIG.publicInputCount),
           pis
         );
 
         let acc = new bn254({ x: VK.ic0.x, y: VK.ic0.y });
-
-        acc = acc.add(VK.ic1.scale(pis[0]));
-        acc = acc.add(VK.ic2.scale(pis[1]));
-        acc = acc.add(VK.ic3.scale(pis[2]));
-        // acc = acc.add(VK.ic4.scale(pis[3]));
-        // acc = acc.add(VK.ic5.scale(pis[4]));
+        switch (CONFIG.publicInputCount) {
+          case 0:
+            // Only ic0, no additional inputs
+            break;
+          case 1:
+            // zkp14 handles [0]: ic1*pis[0]
+            acc = acc.add(VK.ic1.scale(pis[0]));
+            break;
+          case 2:
+            // zkp14 handles [0,1]: ic1*pis[0] + ic2*pis[1]
+            acc = acc.add(VK.ic1.scale(pis[0]));
+            acc = acc.add(VK.ic2.scale(pis[1]));
+            break;
+          case 3:
+            // zkp14 handles [0,1,2]: ic1*pis[0] + ic2*pis[1] + ic3*pis[2]
+            acc = acc.add(VK.ic1.scale(pis[0]));
+            acc = acc.add(VK.ic2.scale(pis[1]));
+            acc = acc.add(VK.ic3.scale(pis[2]));
+            break;
+          case 4:
+            // zkp14 handles [0,1]: ic1*pis[0] + ic2*pis[1] (2+2 distribution)
+            acc = acc.add(VK.ic1.scale(pis[0]));
+            acc = acc.add(VK.ic2.scale(pis[1]));
+            break;
+          case 5:
+            // zkp14 handles [0,1,2]: ic1*pis[0] + ic2*pis[1] + ic3*pis[2] (3+2 distribution)
+            acc = acc.add(VK.ic1.scale(pis[0]));
+            acc = acc.add(VK.ic2.scale(pis[1]));
+            acc = acc.add(VK.ic3.scale(pis[2]));
+            break;
+          case 6:
+            // zkp14 handles [0,1,2]: ic1*pis[0] + ic2*pis[1] + ic3*pis[2] (3+3 distribution)
+            acc = acc.add(VK.ic1.scale(pis[0]));
+            acc = acc.add(VK.ic2.scale(pis[1]));
+            acc = acc.add(VK.ic3.scale(pis[2]));
+            break;
+          default:
+            throw new Error(`Unsupported input count: ${CONFIG.publicInputCount}`);
+        }
 
         // assert that sum ic_i * pis[i] = PI
 
--- src/groth/recursion/zkp15.ts
+++ src/groth/recursion/zkp15.ts
@@ -5,6 +5,7 @@ import { ArrayListHasher } from '../../array_list_hasher.js';
 import { VK } from '../vk_from_env.js';
 import { G1Affine } from '../../ec/index.js';
 import { bn254 } from '../../ec/g1.js';
+import { CONFIG } from '../config.js';
 
 const zkp15 = ZkProgram({
   name: 'zkp15',
@@ -12,11 +13,13 @@ const zkp15 = ZkProgram({
   publicOutput: Field,
   methods: {
     compute: {
-      privateInputs: [G1Affine, G1Affine, Provable.Array(FrC.provable, 5)],
+      privateInputs: [G1Affine, G1Affine, Provable.Array(FrC.provable, CONFIG.publicInputCount)],
       async method(input: Field, PI: G1Affine, acc: G1Affine, pis: Array<FrC>) {
+        // Note: pis.length is validated at compile time by Provable.Array constraint
+
         const pi_hash = Poseidon.hashPacked(G1Affine, PI);
         const pis_hash = Poseidon.hashPacked(
-          Provable.Array(FrC.provable, 5),
+          Provable.Array(FrC.provable, CONFIG.publicInputCount),
           pis
         );
         const acc_hash = Poseidon.hashPacked(G1Affine, acc);
@@ -29,11 +32,40 @@ const zkp15 = ZkProgram({
         );
 
         let accBn = new bn254({ x: acc.x, y: acc.y });
-        accBn = accBn.add(VK.ic4.scale(pis[3]));
-        accBn = accBn.add(VK.ic5.scale(pis[4]));
-
-        accBn.x.assertCanonical().assertEquals(PI.x);
-        accBn.y.assertCanonical().assertEquals(PI.y);
+        switch (CONFIG.publicInputCount) {
+          case 0:
+          case 1:
+          case 2:
+          case 3:
+            // zkp15 handles no inputs - validate acc equals PI directly
+            accBn.x.assertCanonical().assertEquals(PI.x);
+            accBn.y.assertCanonical().assertEquals(PI.y);
+            break;
+          case 4:
+            // zkp15 handles [2,3]: ic3*pis[2] + ic4*pis[3] (2+2 distribution)
+            accBn = accBn.add(VK.ic3.scale(pis[2]));
+            accBn = accBn.add(VK.ic4.scale(pis[3]));
+            accBn.x.assertCanonical().assertEquals(PI.x);
+            accBn.y.assertCanonical().assertEquals(PI.y);
+            break;
+          case 5:
+            // zkp15 handles [3,4]: ic4*pis[3] + ic5*pis[4] (3+2 distribution)
+            accBn = accBn.add(VK.ic4.scale(pis[3]));
+            accBn = accBn.add(VK.ic5.scale(pis[4]));
+            accBn.x.assertCanonical().assertEquals(PI.x);
+            accBn.y.assertCanonical().assertEquals(PI.y);
+            break;
+          case 6:
+            // zkp15 handles [3,4,5]: ic4*pis[3] + ic5*pis[4] + ic6*pis[5] (3+3 distribution)
+            accBn = accBn.add(VK.ic4.scale(pis[3]));
+            accBn = accBn.add(VK.ic5.scale(pis[4]));
+            accBn = accBn.add(VK.ic6.scale(pis[5]));
+            accBn.x.assertCanonical().assertEquals(PI.x);
+            accBn.y.assertCanonical().assertEquals(PI.y);
+            break;
+          default:
+            throw new Error(`Unsupported input count: ${CONFIG.publicInputCount}`);
+        }
 
         return { publicOutput: pis_hash };
       },
