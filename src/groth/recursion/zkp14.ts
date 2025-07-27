28516249d657f6eb1744a06c652b5c415c64df9b
0x471 <0x471@protonmail.com>
2025-07-27 17:41:39 +0100

    feat(groth16): enhance zkp14/zkp15 with dual array parameters

--- src/groth/recursion/zkp14.ts
+++ src/groth/recursion/zkp14.ts
@@ -1,85 +1,62 @@
 import { Field, Poseidon, Provable, ZkProgram } from 'o1js';
-import { Accumulator } from './data.js';
-import { Fp12, FrC } from '../../towers/index.js';
-import { ArrayListHasher } from '../../array_list_hasher.js';
+import { FrC } from '../../towers/index.js';
 import { VK } from '../vk_from_env.js';
 import { G1Affine } from '../../ec/index.js';
 import { bn254 } from '../../ec/g1.js';
-import { CONFIG } from '../config.js';
+import { getDistribution } from '../config.js';
 
-const zkp14 = ZkProgram({
-  name: 'zkp14',
-  publicInput: Field,
-  publicOutput: Field,
-  methods: {
-    compute: {
-      privateInputs: [Provable.Array(FrC.provable, CONFIG.publicInputCount)],
-      async method(input: Field, pis: Array<FrC>) {
-        const pis_hash = Poseidon.hashPacked(
-          Provable.Array(FrC.provable, CONFIG.publicInputCount),
-          pis
-        );
+// Factory function to create zkp14 with correct public input array size
+export function createZkp14(inputCount: number) {
+  const distribution = getDistribution(inputCount);
+  const zkp14InputCount = distribution.zkp14.length;
 
-        let acc = new bn254({ x: VK.ic0.x, y: VK.ic0.y });
-        switch (CONFIG.publicInputCount) {
-          case 0:
-            // Only ic0, no additional inputs
-            break;
-          case 1:
-            // zkp14 handles [0]: ic1*pis[0]
-            acc = acc.add(VK.ic1.scale(pis[0]));
-            break;
-          case 2:
-            // zkp14 handles [0,1]: ic1*pis[0] + ic2*pis[1]
-            acc = acc.add(VK.ic1.scale(pis[0]));
-            acc = acc.add(VK.ic2.scale(pis[1]));
-            break;
-          case 3:
-            // zkp14 handles [0,1,2]: ic1*pis[0] + ic2*pis[1] + ic3*pis[2]
-            acc = acc.add(VK.ic1.scale(pis[0]));
-            acc = acc.add(VK.ic2.scale(pis[1]));
-            acc = acc.add(VK.ic3.scale(pis[2]));
-            break;
-          case 4:
-            // zkp14 handles [0,1]: ic1*pis[0] + ic2*pis[1] (2+2 distribution)
-            acc = acc.add(VK.ic1.scale(pis[0]));
-            acc = acc.add(VK.ic2.scale(pis[1]));
-            break;
-          case 5:
-            // zkp14 handles [0,1,2]: ic1*pis[0] + ic2*pis[1] + ic3*pis[2] (3+2 distribution)
-            acc = acc.add(VK.ic1.scale(pis[0]));
-            acc = acc.add(VK.ic2.scale(pis[1]));
-            acc = acc.add(VK.ic3.scale(pis[2]));
-            break;
-          case 6:
-            // zkp14 handles [0,1,2]: ic1*pis[0] + ic2*pis[1] + ic3*pis[2] (3+3 distribution)
-            acc = acc.add(VK.ic1.scale(pis[0]));
-            acc = acc.add(VK.ic2.scale(pis[1]));
-            acc = acc.add(VK.ic3.scale(pis[2]));
-            break;
-          default:
-            throw new Error(`Unsupported input count: ${CONFIG.publicInputCount}`);
-        }
+  const zkp14 = ZkProgram({
+    name: `zkp14_${inputCount}inputs`,
+    publicInput: Field,
+    publicOutput: Field,
+    methods: {
+      compute: {
+        privateInputs: [Provable.Array(FrC.provable, zkp14InputCount), Provable.Array(FrC.provable, inputCount)],
+        async method(input: Field, zkp14_pis: Array<FrC>, full_pis: Array<FrC>) {
+          const pis_hash = Poseidon.hashPacked(
+            Provable.Array(FrC.provable, inputCount),
+            full_pis
+          );
 
-        // assert that sum ic_i * pis[i] = PI
+          let acc = new bn254({ x: VK.ic0.x, y: VK.ic0.y });
 
-        const acc_aff = new G1Affine({
-          x: acc.x.assertCanonical(),
-          y: acc.y.assertCanonical(),
-        });
-        const acc_hash = Poseidon.hashPacked(G1Affine, acc_aff);
+          // Handle inputs based on distribution strategy
+          for (let i = 0; i < zkp14InputCount; i++) {
+            const icIndex = distribution.zkp14[i] + 1; // ic1, ic2, etc.
+            const icPoint = VK.getIcPoint(icIndex);
+            if (!icPoint) {
+              throw new Error(`Missing IC point ic${icIndex} for zkp14 input ${i}`);
+            }
+            acc = acc.add(icPoint.scale(zkp14_pis[i]));
+          }
 
-        return {
-          publicOutput: Poseidon.hashPacked(Provable.Array(Field, 3), [
+          const acc_aff = new G1Affine({
+            x: acc.x.assertCanonical(),
+            y: acc.y.assertCanonical(),
+          });
+          const acc_hash = Poseidon.hashPacked(G1Affine, acc_aff);
+
+          const publicOutput = Poseidon.hashPacked(Provable.Array(Field, 3), [
             input,
             pis_hash,
             acc_hash,
-          ]),
-        };
+          ]);
+
+
+          return { publicOutput };
+        },
       },
     },
-  },
-});
+  });
 
-const ZKP14Proof = ZkProgram.Proof(zkp14);
+  return { zkp14, ZKP14Proof: ZkProgram.Proof(zkp14) };
+}
+
+// Default export for backwards compatibility (can be removed later)
+const { zkp14, ZKP14Proof } = createZkp14(5); // Default to 5 inputs for Risc0
 export { ZKP14Proof, zkp14 };
