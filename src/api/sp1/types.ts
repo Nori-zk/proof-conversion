69055dcb7e31b66040c338055242617eb5a2c7dd
Jonathan Kelsey <jonathan.s.kelsey@gmail.com>
2026-01-27 13:39:02 +0000

    FIX: removing sp1 specific guards as the schema validation now does everything.

--- src/api/sp1/types.ts
+++ src/api/sp1/types.ts
@@ -1,4 +1,4 @@
-import type { Groth16Bn254Proof, O1jsProof, O1jsVK, PlonkBn254Proof, SP1Proof, SP1ProofWithPublicValues, SP1PublicValues } from "pairing-utils/pkg/pairing_utils.js";
+import type { Groth16Bn254Proof, O1jsProof, O1jsVK, SP1Proof, SP1ProofWithPublicValues, SP1PublicValues } from "pairing-utils/pkg/pairing_utils.js";
 import { sp1Groth16InputSchema, sp1PlonkInputSchema } from "../validation/sp1/schema.js";
 
 // Types ===================================================================================
@@ -30,24 +30,3 @@ export type SP1ProofWithPublicValuesGroth16NoTee = Omit<SP1ProofWithPublicValues
 // export const sp1ArgKeys = ['hexPi', 'programVK', 'encodedProof'] as const; // Removed as this is unsafe until the TEE modification have been tested
 export const sp1PlonkObjKeys = Object.keys(sp1PlonkInputSchema) as (keyof Sp1Input)[];
 export const sp1Groth16ObjKeys = Object.keys(sp1Groth16InputSchema) as (keyof Sp1Input)[];
-
-// Sp1 specific Guards ======================================================================
-
-export function isSp1PlonkProof(proof: SP1Proof): proof is { Plonk: PlonkBn254Proof } {
-  return 'Plonk' in proof;
-}
-
-export function isSp1Groth16Proof(proof: SP1Proof): proof is { Groth16: Groth16Bn254Proof } {
-  return 'Groth16' in proof;
-}
-
-export function isSP1ProofWithPublicValuesGroth16NoTee(obj: Sp1Input): obj is SP1ProofWithPublicValuesGroth16NoTee {
-  return (
-    'Groth16' in obj.proof && 
-    obj.tee_proof === null
-  );
-}
-
-export function isTeeSp1Proof(obj: Sp1Input): boolean {
-  return obj.tee_proof !== null && obj.tee_proof !== undefined;
-}
