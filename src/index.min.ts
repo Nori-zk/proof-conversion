5407e652e7099d8ee05812c219b71ea2ae03fbda
Jonathan Kelsey <jonathan.s.kelsey@gmail.com>
2026-01-28 16:50:17 +0000

    CHORE: Started working on exports

--- src/index.min.ts
+++ src/index.min.ts
@@ -1,5 +1,14 @@
+// All exports in this file are free of Node.js
+
+// Types
+
+export type * from './compute/types.js';
+
+// Utilities
+
 export { parsePublicInputsProvable as parsePlonkPublicInputsProvable } from './plonk/parse_pi.js';
 export { wordToBytes } from './sha/utils.js';
 export { NodeProofLeft } from './structs.js';
 export { FrC } from './towers/fr.js';
 export { DeferredPromise } from './utils/DeferredPromise.js';
+
--- src/index.ts
+++ src/index.ts
@@ -1,19 +1,27 @@
 
 
-// Safe / side-effect-free
+// Node.js dep free ===============================================================================
+
+// Types
+
+export type * from './compute/types.js';
+export type { Sp1Input as Sp1 } from './api/validation/sp1/schema.js';
+
+// Utilities
+
 export { parsePublicInputsProvable as parsePlonkPublicInputsProvable } from './plonk/parse_pi.js';
 export { wordToBytes } from './sha/utils.js';
 export { NodeProofLeft } from './structs.js';
 export { FrC } from './towers/fr.js';
 export { DeferredPromise } from './utils/DeferredPromise.js';
 
+// Node.js ========================================================================================
+
 // Api
 export { performSp1Plonk } from './api/sp1/plonk.js';
 export { performRisc0Groth16 } from './api/risc0/groth16.js';
 export { performSnarkjsGroth16 } from './api/snarkjs/groth16.js';
 export { performSp1Groth16 } from './api/sp1/groth16.js';
-
-export type { Sp1Input as Sp1 } from './api/validation/sp1/schema.js';
 
 // Compute
 export { ComputationalPlanExecutor } from './compute/executor.js';
