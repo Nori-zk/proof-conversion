dc52839a4c988e969212d64870b1aeb2367f7a10
Jonathan Kelsey <jonathan.s.kelsey@gmail.com>
2026-02-20 14:50:53 +0000

    FEAT: adding script to save new groth16 vk as bin

---
+++ pairing-utils/src/bin/save_sp1_groth16_bin.rs
@@ -0,0 +1,8 @@
+use std::fs;
+
+use sp1_verifier::GROTH16_VK_BYTES;
+
+fn main() {
+    let path = concat!(env!("CARGO_MANIFEST_DIR"), "/sp1_v6_groth16_vk.bin");
+    fs::write(path, *GROTH16_VK_BYTES).expect("Failed to write sp1_v6_groth16_vk.bin");
+}
